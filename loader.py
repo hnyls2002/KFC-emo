2d68ce5d497a55141def22b95f2dda1e0625209f
hnyls2002 <hnyls2002@sjtu.edu.cn>
2023-05-20 15:27:06 +0800

    feat(emo_type)

--- loader.py
+++ loader.py
@@ -1,10 +1,11 @@
+import json
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import torch
 
 
-def load_data(data_path="data/"):
+def load_data(data_path="data/", emo_type="goemotion"):
     print(">>>>>>>>>>>> Loading Data >>>>>>>>>>>>")
     # Check if processed CSV files exist
     if os.path.exists(data_path + "train.csv") and os.path.exists(data_path + "dev.csv") and os.path.exists(data_path + "test.csv"):
@@ -48,31 +49,59 @@ def load_data(data_path="data/"):
         dev.to_csv(data_path + "dev.csv", index=False)
         test.to_csv(data_path + "test.csv", index=False)
 
-    print(">>>>>>>>>>>> The size of train/dev/test set is: ", train.shape, dev.shape, test.shape)
+    print(">>>>>>>>>>>> The size of train/dev/test set is: ",
+          train.shape, dev.shape, test.shape)
 
     # Read the emotions list
     with open(data_path + "emotions.txt", "r") as f:
         emotion_list = [line.strip() for line in f.readlines()]
+    with open(data_path + "sentiments.txt", "r") as f:
+        sentiment_list = [line.strip() for line in f.readlines()]
+    with open(data_path + "ekmans.txt", "r") as f:
+        ekman_list = [line.strip() for line in f.readlines()]
+
+    # sentiment mapping
+    json_file = open(data_path + "sentiment_mapping.json", 'r')
+    sentiment_mapping = json.load(json_file)
+    for dataset in (train, dev, test):
+        for sentiment in sentiment_list:
+            dataset[sentiment] = pd.Series([0] * len(dataset))
+            for emo in sentiment_mapping[sentiment]:
+                dataset[sentiment] |= dataset[emo]
+        dataset["senti_sum"] = dataset[sentiment_list].apply(
+            lambda x: sum([x[sentiment] for sentiment in sentiment_list]), axis=1)
+
+    # ekman mapping
+    json_file = open(data_path + "ekman_mapping.json", 'r')
+    ekman_mapping = json.load(json_file)
+    for dataset in (train, dev, test):
+        for ekman in ekman_list:
+            dataset[ekman] = pd.Series([0] * len(dataset))
+            for emo in ekman_mapping[ekman]:
+                dataset[ekman] |= dataset[emo]
+        dataset["ekman_sum"] = dataset[ekman_list].apply(
+            lambda x: sum([x[ekman] for ekman in ekman_list]), axis=1)
+
+    # write first 100 line to test.txt
+    # dataset.head(100).to_csv(data_path + "test.txt", index=False)
 
     for dataset in (train, dev, test):
-        dataset["label"] = dataset[emotion_list].apply(
-            lambda x: torch.tensor([x[emotion] for emotion in emotion_list]), axis=1)
-        dataset["label_sum"] = dataset["label"].apply(
-            lambda x: torch.sum(x))
-        # for _, row in dataset.iterrows():
-        #     for idx, emo in enumerate(emotion_list):
-        #         if row[emo] != row["label"][idx]:
-        #             print(
-        #                 "\x1b[31m" + ">>>>>>>>>>>> Warning: Inconsistent label detected in the following example:" + "\x1b[0m")
-        #             print(row["text"])
-        #     if row["label_sum"] == 0:
-        #         print(
-        #             "\x1b[31m" + ">>>>>>>>>>>> Warning: No emotion detected in the following example:" + "\x1b[0m")
-        #         print(row["text"])
+        if emo_type == "goemotion":
+            dataset["label"] = dataset[emotion_list].apply(
+                lambda x: torch.tensor([x[emotion] for emotion in emotion_list]), axis=1)
+        elif emo_type == "sentiment":
+            dataset["label"] = dataset[sentiment_list].apply(
+                lambda x: torch.tensor([x[sentiment] for sentiment in sentiment_list]), axis=1)
+        elif emo_type == "ekman":
+            dataset["label"] = dataset[ekman_list].apply(
+                lambda x: torch.tensor([x[ekman] for ekman in ekman_list]), axis=1)
+        else:
+            print("Error: Invalid emo_type")
+            exit(1)
 
     # print example
     print("======================================example==========================================")
-    print(train.head()[["text", "label_sum", "label"]])
+    print(train.head()[["text", "label"]])
     print("=======================================================================================")
 
     return train, dev, test
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # load settings
 print(">>>>>>>>>>>> Loading settings... ")
 saving_dir = "./saving/"
-exp_name = "exp-10"
+exp_name = "exp-11"
 hypara = try_load_hypara(exp_name)
 if hypara is None:
     print("No such experiment!")
@@ -43,9 +43,12 @@ drpout = hypara['drpout']
 fixed_threshold = hypara['threshold']
 freeze_flag = hypara['freeze'] == "True"
 is_fixed_threshold = hypara['is_fixed_threshold'] == "True"
+emo_type = hypara['emo_type']
+
+print(">>>>>>>>>>>> The emotion list is: ", emo_list)
 
 # load data
-train, dev, test = load_data()
+train, dev, test = load_data(emo_type=emo_type)
 
 # load already runned epochs
 runned_epochs = len(load_res(exp_name, emo_list))
